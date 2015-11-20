48229b2ebec1ee51f7152e4a45a7aefad1ca918b
rhquant <travis.kasinger@yodle.com>
2015-11-20 10:27:02 -0600

    Initial Commit

---
+++ OpenCase.py
@@ -0,0 +1,21 @@
+from simple_salesforce import Salesforce
+import pymssql
+import pyodbc
+import json
+
+sf=Salesforce(username='', password='',security_token='')
+done = False
+def statuscheck():
+    ##cid='05338520'
+    cid_ask = input(str("Enter Case Number to get Status: " ))
+    try:
+        a=sf.query("SELECT Status, Owner.Name FROM case where CaseNumber = '{0}'".format(cid_ask))
+        status = a['records'][0]['Status']
+        owner = a['records'][0]['Owner']['Name']
+        return("Case {0} is owned by {1} and is in status {2}".format(cid_ask, owner, status))
+        global done
+        done = True
+    except:
+        print("The case number you have entered is invalid.")
+while (done == False):
+    statuscheck()
