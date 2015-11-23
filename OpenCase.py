281e60e552a9b881705816bca2030575162f979a
rhquant <tkasinger@gmail.com>
2015-11-23 15:31:13 -0600

    Added Functionality to allow user input

--- OpenCase.py
+++ OpenCase.py
@@ -1,24 +1,26 @@
+#Travis Kasinger
 from simple_salesforce import Salesforce
 import pymssql
 import pyodbc
 import json
 
-sf=Salesforce(username='', password='',security_token='')
 done = False
 def statuscheck():
-    ##cid='05338520'
+    global done
+    uname = input(str("Enter Username: "))
+    pword = input(str("Enter password: "))
+    key = input(str("Enter API key: "))
+    sf=Salesforce(username=uname, password=pword, security_token=key)
     cid_ask = input(str("Enter Case Number to get Status: " ))
     try:
         a=sf.query("SELECT Status, Owner.Name FROM case where CaseNumber = '{0}'".format(cid_ask))
         status = a['records'][0]['Status']
         owner = a['records'][0]['Owner']['Name']
-        return("Case {0} is owned by {1} and is in status {2}".format(cid_ask, owner, status))
-        global done
+        print("Case {0} is owned by {1} and is in status {2}".format(cid_ask, owner, status))
         done = True
     except:
         print("The case number you have entered is invalid.")
+
 while (done == False):
     statuscheck()
 
-
-#what the fudge
